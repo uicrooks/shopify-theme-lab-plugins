a0a80d436e878b6a5c49ac2f85433304fdc0d153
Sergej Samsonenko <contact@sergej.codes>
2021-03-28 17:54:57 +0200

    set default value of indicator to false

--- packages/reloader/config.js
+++ packages/reloader/config.js
@@ -5,5 +5,5 @@ module.exports = {
   serverPort: package.config && package.config.reloader.serverPort || 5000, // port for communication with Shopify themekit
   webSocketPort: package.config && package.config.reloader.webSocketPort || 5050, // port for communication with reloader client
   delay: package.config && package.config.reloader.delay || 2000, // delay the auto-reload by specified milliseconds
-  indicator: package.config && package.config.reloader.indicator || true // show HTML status indicator
+  indicator: package.config && package.config.reloader.indicator || false // show HTML element on status change
 }
