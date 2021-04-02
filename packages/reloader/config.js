5b38cceab8d5db0cb9b71687958a8f6eda79bb55
Sergej Samsonenko <contact@sergej.codes>
2021-04-02 17:44:42 +0200

    feat: add https secure server option

--- packages/reloader/config.js
+++ packages/reloader/config.js
@@ -4,6 +4,7 @@ const package = require('../../package.json')
 module.exports = {
   serverPort: package.config && package.config.reloader.serverPort || 5000, // port for communication with Shopify themekit
   webSocketPort: package.config && package.config.reloader.webSocketPort || 5050, // port for communication with reloader client
+  webSocketSecure: package.config && package.config.reloader.webSocketSecure || false, // run an https server for websocket connection
   delay: package.config && package.config.reloader.delay || 2000, // delay the auto-reload by specified milliseconds
   indicator: package.config && package.config.reloader.indicator || false // show HTML element on status change
 }
